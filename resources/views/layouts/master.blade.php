e3f5d4794f160c66f73d3f3dfb9a3f559ea1bb35
dawa.tshering <dawa.tshering@thimphutechpark.bt>
2022-01-16 12:32:43 +0600

    dist-folder-added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -158,7 +158,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Thimphu Futsal</span>
     </a>
 
     <!-- Sidebar -->
@@ -169,7 +169,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">nyuethyue deadpool</a>
         </div>
       </div>
 
